3d87523741c1169d66d854b04de19b27dd805ae7
Paichien <b8350580@gmail.com>
2023-06-18 01:00:55 +0800

    save

--- index.js
+++ index.js
@@ -11,11 +11,25 @@ return inquirer
         type:'list',
         name: 'todo',
         message: 'What would you like to do?',
-        choices: ['View All Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
     }
-]).then(({todo})=>{
-    if(todo==='View All Employee'){
-        emp.all()
+]).then(async ({todo})=>{
+    if(todo==='View All Employees'){
+        console.log(todo);
+        const allEmployees= await emp.all();
+        console.table(allEmployees);
+    }else if(todo==='Add Employee'){
+        console.log(todo);
+        return inquirer
+        .prompt([
+        {
+        type:'input',
+        name: 'firstName',
+        message: "What is the employee's first name?",
+        }
+        ]).then(({name})=>{
+            console.log(name);
+        })
     }
 })
 
